dbd16004f27952995cbc432dc6f8561243e80d29
xiangxiang-en <138411810+xiangxiang-en@users.noreply.github.com>
2023-07-05 17:41:26 +0800

    Web API

---
+++ DemoWebAPI_KT069/DemoWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using refs.EmployeeInformation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoWebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        public object KT069()
+        {
+            return EmployeeInfo.Get();
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostEmployee([FromBody] Employee employee)
+        {
+            string result = $"EmpNo: {employee.EmpNo}, EmpCode: {employee.EmpCode}, EmpName: {employee.EmpName}";
+            return Ok(result);
+        }
+    }
+
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string EmpCode { get; set; }
+        public string EmpName { get; set; }
+    }
+}
---
+++ DemoWebAPI_KT069/DemoWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoWebAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private List<string> values = new List<string>();
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "1", "2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "Hello World ";
+
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+            values.Add(value);
+
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
