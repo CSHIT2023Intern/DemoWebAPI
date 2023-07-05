24b003bc74aa87a04a97cc94775bb5f3f3d9206f
Mixi1021 <mixiwu1021@gmail.com>
2023-07-05 23:01:33 +0800

    HttpPost

--- KT063/0705_DemoWebAPI/Controllers/EmployeeController.cs
+++ KT063/0705_DemoWebAPI/Controllers/EmployeeController.cs
@@ -11,9 +11,23 @@ namespace _0705_DemoWebAPI.Controllers
     public class EmployeeController : ApiController
     {
         [HttpGet]
-        public object Employee()
+        public object Employee1()
         {
             return EmployeeInfo.Get();
+        }
+
+        public class Employee
+        {
+            public int EmpNo { get; set; }
+            public string EmpCode { get; set; }
+            public string EmpName { get; set; }
+        }
+
+        [HttpPost] // 新增 [HttpPost] 屬性修飾器
+        public string New(Employee employee)
+        {
+            string result = $"EmpNo: {employee.EmpNo}, EmpCode: {employee.EmpCode}, EmpName: {employee.EmpName}";
+            return result;
         }
     }
 }
