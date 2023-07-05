9cfdfc84a6bc9c2b4f162ec8645cab8843a5f448
mzhi8 <tamijelly0315@gmail.com>
2023-07-05 15:46:23 +0800

    get

---
+++ DemoWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using refs.EmployeeInformation;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+
+namespace DemoWebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        public object Employee()
+        {
+            return EmployeeInfo.Get();
+        }
+
+        
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+    }
+}
