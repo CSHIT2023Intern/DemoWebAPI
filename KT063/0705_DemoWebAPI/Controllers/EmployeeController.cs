03952f22fec0f846ae8450fc2fc67c334d4ba879
Mixi1021 <mixiwu1021@gmail.com>
2023-07-05 13:28:38 +0800

    HttpGet

---
+++ KT063/0705_DemoWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using refs.EmployeeInformation;
+
+namespace _0705_DemoWebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        public object Employee()
+        {
+            return EmployeeInfo.Get();
+        }
+    }
+}
