76f93cb396b6f0a1b91ee034fa454914a4600ec2
Pau1l24 <snowxpa24@gmail.com>
2023-07-06 17:15:39 +0800

    少NEW
    CRUD非NEW方法

--- DemoWebAPI/Controllers/EmployeeController.cs
+++ DemoWebAPI/Controllers/EmployeeController.cs
@@ -10,56 +10,123 @@ namespace DemoWebAPI.Controllers
 {
     public class EmployeeController : ApiController
     {
-        //public class Employee
-        //{
-        //    public int empNo { get; set; }
-        //    public string empCode { get; set; }
-        //    public string empName { get; set; }
-        //}
-        //public static List<Employee> _employee = new List<Employee>();
+        private static List<Employee> employees = new List<Employee>();
+
         [HttpGet]
-        public object Employee()
+        public IHttpActionResult GetAllEmployees()
         {
-            return EmployeeInfo.Get();
+            var allEmployees = EmployeeInfo.Get();
+            var combinedEmployees = allEmployees.Concat(employees).ToList();
+            return Ok(combinedEmployees);
         }
 
-        //public object EmployeeGet()
-        //{
-        //    Employee _employee = new Employee();
-        //    var employeeData = EmployeeInfo.Get();
-        //    List<Employee> employees = employeeData.Select(e => new Employee
-        //    {
-        //        empNo = e.empNo,
-        //        empCode = e.empCode,
-        //        empName = e.empName,
-
-        //    }).ToList();
-        //    return employees;
-        //}
+        [HttpGet]
+        public IHttpActionResult GetEmployeeById(int id)
+        {
+            var allEmployees = EmployeeInfo.Get();
+            var employee = allEmployees.Concat(employees).FirstOrDefault(e => e.empNo == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
 
         [HttpPost]
-        public string Employee(Employee emp)
+        public IHttpActionResult CreateEmployee(Employee employee)
         {
-            return EmployeeInfo.New(emp.empNo, emp.empCode, emp.empName);
+            employees.Add(employee);
+            return CreatedAtRoute("DefaultApi", new { id = employee.empNo }, employee);
         }
 
-        //public IHttpActionResult Post([FromBody] Employee employee)
-        //{
+        [HttpPut]
+        public IHttpActionResult UpdateEmployee(int id, Employee updatedEmployee)
+        {
+            var employee = employees.Find(e => e.empNo == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            employee.empCode = updatedEmployee.empCode;
+            employee.empName = updatedEmployee.empName;
+            return Ok(employee);
+        }
 
-        //    string result = $"EmpNo: {employee.empNo}, EmpCode: {employee.empCode}, EmpName: {employee.empName}";
-        //    return Ok(result);
-        //}
-
-        //public IHttpActionResult Insert([FromBody] Employee parameter)
-        //{
-        //    _employee.Add(new Employee
-        //    {
-        //        empNo = parameter.empNo,
-        //        empCode = parameter.empCode,
-        //        empName = parameter.empName
-        //    });
-        //    string result = $"EmpNo: {parameter.empNo}, EmpCode: {parameter.empCode}, EmpName: {parameter.empName}";
-        //    return Ok(result);
-        //}
+        [HttpDelete]
+        public IHttpActionResult DeleteEmployee(int id)
+        {
+            var employee = employees.Find(e => e.empNo == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            employees.Remove(employee);
+            return Ok();
+        }
     }
 }
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using System.Web.Http;
+//using refs.EmployeeInformation;
+
+//namespace DemoWebAPI.Controllers
+//{
+//    public class EmployeeController : ApiController
+//    {
+//        //public class Employee
+//        //{
+//        //    public int empNo { get; set; }
+//        //    public string empCode { get; set; }
+//        //    public string empName { get; set; }
+//        //}
+//        //public static List<Employee> _employee = new List<Employee>();
+//        [HttpGet]
+//        public object Employee()
+//        {
+//            return EmployeeInfo.Get();
+//        }
+
+//        //public object EmployeeGet()
+//        //{
+//        //    Employee _employee = new Employee();
+//        //    var employeeData = EmployeeInfo.Get();
+//        //    List<Employee> employees = employeeData.Select(e => new Employee
+//        //    {
+//        //        empNo = e.empNo,
+//        //        empCode = e.empCode,
+//        //        empName = e.empName,
+
+//        //    }).ToList();
+//        //    return employees;
+//        //}
+
+//        [HttpPost]
+//        public string Employee(Employee emp)
+//        {
+//            return EmployeeInfo.New(emp.empNo, emp.empCode, emp.empName);
+//        }
+
+//        //public IHttpActionResult Post([FromBody] Employee employee)
+//        //{
+
+//        //    string result = $"EmpNo: {employee.empNo}, EmpCode: {employee.empCode}, EmpName: {employee.empName}";
+//        //    return Ok(result);
+//        //}
+
+//        //public IHttpActionResult Insert([FromBody] Employee parameter)
+//        //{
+//        //    _employee.Add(new Employee
+//        //    {
+//        //        empNo = parameter.empNo,
+//        //        empCode = parameter.empCode,
+//        //        empName = parameter.empName
+//        //    });
+//        //    string result = $"EmpNo: {parameter.empNo}, EmpCode: {parameter.empCode}, EmpName: {parameter.empName}";
+//        //    return Ok(result);
+//        //}
+//    }
+//}
