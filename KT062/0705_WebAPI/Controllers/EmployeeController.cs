96d87b3dcb656b1ee4f4c97f85bfb3f90fecf941
Jessica-0112 <ica3431652@gmail.com>
2023-07-06 09:02:42 +0800

    HttpPost

--- KT062/0705_WebAPI/Controllers/EmployeeController.cs
+++ KT062/0705_WebAPI/Controllers/EmployeeController.cs
@@ -10,10 +10,33 @@ namespace _0705_WebAPI.Controllers
 {
     public class EmployeeController : ApiController
     {
+        // Get
         [HttpGet]
         public object Employee()
         {
             return EmployeeInfo.Get();
         }
+
+        // 建立一個Employee's List
+        private readonly List<Employee> employeeList = new List<Employee>
+        {
+        };
+
+        // Post
+        [HttpPost]
+        public string Post([FromBody]Employee employee)
+        {
+            // 新增employee
+            employeeList.Add(employee);
+            //var newEmp = EmployeeInfo.New(int empNo, string empCode, string empName)->string;
+            var newEmp = $"EmpNo: {employee.empNo}, EmpCode: {employee.empCode}, EmpName: {employee.empName}";
+            return newEmp;
+        }
+    }
+    public class Employee
+    {
+        public int empNo { get; set; }
+        public string empCode { get; set; }
+        public string empName { get; set; }
     }
 }
