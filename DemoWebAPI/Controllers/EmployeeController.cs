5e9faa6beb25113ccb603153c9a960f2dee1b530
chialia <barbiemail01@gmail.com>
2023-07-06 13:15:05 +0800

    加入List
    可以讓POST的資料GET回來

--- DemoWebAPI/Controllers/EmployeeController.cs
+++ DemoWebAPI/Controllers/EmployeeController.cs
@@ -10,16 +10,28 @@ namespace DemoWebAPI.Controllers
 {
     public class EmployeeController : ApiController
     {
+        //可以讀取剛創建的資料，但並非與原始資料一起
+        private static List<Employee> employees = new List<Employee>();
+
+        //[HttpGet]
+        //public object Employee1()
+        //{
+        //    return EmployeeInfo.Get();
+        //}
+
         [HttpGet]
-        public object Employee1()
+        public object Employee2()
         {
-            return EmployeeInfo.Get();
+            //EmployeeInfo.Get()
+            return Ok(employees); //可以讀取剛創建的資料，但並非與原始資料一起
         }
+
 
         [HttpPost]
         public IHttpActionResult POST(Employee employee)
         {
             var result = "EmpNo:" + employee.EmpNo + "," + "EmpCode:" + employee.EmpCode + "," + "EmpName:" + employee.EmpName;
+            employees.Add(employee);//可以讀取剛創建的資料，但並非與原始資料一起
             return Ok(result);
         }
 
