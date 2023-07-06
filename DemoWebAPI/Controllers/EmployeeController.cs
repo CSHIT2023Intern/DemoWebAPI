394c606b00f4dd4c208538fa1a72e70e4400e28f
ZZoe-tech <zoe127950@gmail.com>
2023-07-06 13:42:43 +0800

    KT061
    07061342

---
+++ DemoWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using refs.EmployeeInformation;
+
+namespace DemoWebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        public object Employee()
+        {
+            return EmployeeInfo.Get();
+        }
+
+        [HttpPost]
+        public string CreateEmployee(Employee employee)
+        {
+            int empNo = employee.empNo;
+            string empCode = employee.empCode;
+            string empName = employee.empName;
+
+            string result = EmployeeInfo.New(empNo, empCode, empName);
+
+            return result;
+        }
+    }
+}
