f5abe57172cd99af3acab1a3ecaa317a137c24ba
Acoppopp <u1095205@hk.edu.tw>
2023-07-06 16:51:44 +0800

    0706

--- DemoWebAPI/Controllers/EmployeeController.cs
+++ DemoWebAPI/Controllers/EmployeeController.cs
@@ -1,45 +1,60 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using refs.EmployeeInformation;
 using System.Web.Http;
-using Newtonsoft.Json;
-using refs.EmployeeInformation;
 
-namespace DemoWebAPI.Controllers
 {
     public class EmployeeController : ApiController
     {
-        private static List<Employee> employees = new List<Employee>(); // 儲存新增的員工資料
-/*      範例一 查尋資料
+        //  private static List<Employee> employees = new List<Employee>(); // 儲存新增的員工資料
+        //  範例一 查尋資料
         [HttpGet]
         public object Employee()
         {
             return EmployeeInfo.Get();
         }
-*/
+
         // GET，查詢新增員工資訊
-        [HttpGet]
-        public IEnumerable<Employee> Employee()
-        {
-            return employees;
-        }
+        /*       [HttpGet]
+               public IEnumerable<Employee> Employee()
+               {
+                   return employees;
+               }
+       */
         // POST，新增員工資訊
         [HttpPost]
-        public IHttpActionResult PostEmployee([FromBody] Employee employee)
+        /*public IHttpActionResult PostEmployee(Employee employee)
         {
             employees.Add(employee); // 將新增的員工資料加入到列表中
             return Ok(employee);
         }
+        */
+        //方法一:
+        // 加入[HttpPost]特徵
+        // 回傳 string 型態
+        /*
+         public string Employee(Employee emp)
+         {
+             return EmployeeInfo.New(emp.empNo, emp.empCode, emp.empName);
+         }
+       */
+        // 方法二:
+        // 回傳資料型態使用 IHttpActionResult
+        // 方法名稱宣告為Post 
+
+        public IHttpActionResult Post(Employee emp)
+        {
+            return Ok(EmployeeInfo.New(emp.empNo, emp.empCode, emp.empName));
+        }
+
     }
     // 反序列化類別
     ///api/Employee
     // {"EmpNo":"0","EmpCode":"App-01","EmpName":"Dev_02"}
-    public class Employee
-    {
-        public int EmpNo { get; set; }
-        public string EmpCode { get; set; }
-        public string EmpName { get; set; }
-    }
+    /* public class Employee
+     {
+         public int EmpNo { get; set; }
+         public string EmpCode { get; set; }
+         public string EmpName { get; set; }
+     }
+    */
 }
 
